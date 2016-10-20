b2fd965a9ba949306cff19a945bef22e2c3a3f30
Iftach Yakar <fatach@gmail.com>
2016-10-20 14:56:45 +0300

    Changed spaceBetween default to original

--- ActionButtonItem.js
+++ ActionButtonItem.js
@@ -13,7 +13,7 @@ export default class ActionButtonItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      spaceBetween: this.props.spaceBetween || 17,
+      spaceBetween: this.props.spaceBetween || 15,
       alignItems: alignItemsMap[this.props.position]
     };
 

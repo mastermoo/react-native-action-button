a80144e36dae34f9de7b015a5e87457dc5dc276e
Iris Chang <ichang@peakcontact.com>
2016-11-01 16:34:11 -0400

    Correct spelling mistake

--- ActionButtonItem.js
+++ ActionButtonItem.js
@@ -40,7 +40,7 @@ export default class ActionButtonItem extends Component {
           alignItems: this.state.alignItems,
           marginBottom: this.props.verticalOrientation === 'up' ? margin : 0,
           marginTop: this.props.verticalOrientation === 'down' ? margin : 0,
-          marginHoriztonal: 8,
+          marginHorizontal: 8,
           opacity: this.props.anim,
           transform: [
             { translateX },
